86c69653a0d28f667dac6217d1a28d9581191d6d
adyasena <adyasena01@mail.ugm.ac.id>
2023-10-17 20:20:23 +0700

    feat: add sticky to navbar

--- src/components/layout/DefaultLayout.js
+++ src/components/layout/DefaultLayout.js
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function DefaultLayout({ children }) {
     return (
-        <div className="bg-earth-50 font-sans w-full h-full">
+        <div className="bg-earth-50 font-sans w-full h-full overflow-auto">
             <Navbar />
             <div className="h-full">{children}</div>
             <Footer />
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,8 +3,8 @@ import { FaMagnifyingGlass, FaBars } from "react-icons/fa6";
 
 export default function Navbar() {
     return (
-        <div className="z-10 inset-0 fixed w-full mx-auto text-center">
-            <div className="text-leaf-900 py-8 w-full px-24 mx-auto flex justify-between items-center font-serif h-20">
+        <div className="z-10 inset-0 sticky w-full mx-auto text-center h-20">
+            <div className="text-leaf-900 py-8 w-full px-24 mx-auto flex justify-between items-center font-serif">
                 <FaMagnifyingGlass className="text-xl hover:text-leaf-500 hover:drop-shadow transition" />
                 <span className="font-bold">Literasi Jamu Digital</span>
                 <FaBars className="text-xl" />
