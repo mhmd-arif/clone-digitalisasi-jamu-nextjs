3b07a808d466790cfa0799596a7ca29cb7daf4ca
mhmd-arif <mhmdarif.developer@gmail.com>
2023-10-18 09:41:53 +0700

    feat(page): update ui

--- src/app/bahan/simplisia/page.jsx
+++ src/app/bahan/simplisia/page.jsx
@@ -1,41 +1,42 @@
 import React from "react";
 import Card from "@/components/Card";
+import Link from "next/link";
 
 export default function Simplisia() {
     const cardData = [
         {
             href: "/bahan/simplisia/simplisia1",
-            img: "Gambar simplisia 1",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
             title: "simplisia 1",
             subtitle: "nama latin simplisia 1",
         },
         {
             href: "/bahan/simplisia/simplisia2",
-            img: "Gambar simplisia 2",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
             title: "simplisia 2",
             subtitle: "nama latin simplisia 2",
         },
         {
             href: "/bahan/simplisia/simplisia3",
-            img: "Gambar simplisia 3",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
             title: "simplisia 3",
             subtitle: "nama latin simplisia 3",
         },
         {
             href: "/bahan/simplisia/simplisia4",
-            img: "Gambar simplisia 4",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
             title: "simplisia 4",
             subtitle: "nama latin simplisia 4",
         },
         {
             href: "/bahan/simplisia/simplisia5",
-            img: "Gambar simplisia 5",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
             title: "simplisia 5",
             subtitle: "nama latin simplisia 5",
         },
         {
             href: "/bahan/simplisia/simplisia6",
-            img: "Gambar simplisia 6",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
             title: "simplisia 6",
             subtitle: "nama latin simplisia 6",
         },
@@ -43,13 +44,22 @@ export default function Simplisia() {
 
     return (
         <div className="w-10/12 h-full mx-auto flex flex-col items-center gap-6 font-serif">
-            <span className="text-h3 text-leaf-900 font-bold pt-4">Simplisia</span>
+            <span className="self-start text-base"> home/bahan/simplisia</span>
+            <div className="flex gap-x-2">
+                <Link href="/bahan/tumbuhanobat">
+                    <span className="text-h3 text-leaf-900 font-bold pt-4">Tumbuhan Obat</span>
+                </Link>
+                <Link href="/bahan/simplisia">
+                    <span className="text-h3 text-leaf-900 font-bold pt-4">Simplisia</span>
+                </Link>
+            </div>
+
             <div className="grid grid-cols-4 text-xl text-earth-50 gap-6 w-full">
                 {cardData.map((card, index) => (
                     <Card key={index} href={card.href} title={card.title} subtitle={card.subtitle} img={card.img}>
                     </Card>
                 ))}
             </div>
-        </div>
+        </div >
     )
 }
--- src/app/bahan/tumbuhanobat/page.jsx
+++ src/app/bahan/tumbuhanobat/page.jsx
@@ -1,55 +1,66 @@
 import React from "react";
 import Card from "@/components/Card";
+import Link from "next/link";
 
 export default function TumbuhanObat() {
     const cardData = [
         {
             href: "/bahan/tumbuhanobat/tumbuhanobat1",
-            img: "Gambar tumbuhanobat 1",
-            title: "tumbuhanobat 1",
-            subtitle: "nama latin tumbuhanobat 1",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
+            title: "tumbuhan obat 1",
+            subtitle: "nama latin tumbuhan obat 1",
         },
         {
             href: "/bahan/tumbuhanobat/tumbuhanobat2",
-            img: "Gambar tumbuhanobat 2",
-            title: "tumbuhanobat 2",
-            subtitle: "nama latin tumbuhanobat 2",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
+            title: "tumbuhan obat 2",
+            subtitle: "nama latin tumbuhan obat 2",
         },
         {
             href: "/bahan/tumbuhanobat/tumbuhanobat3",
-            img: "Gambar tumbuhanobat 3",
-            title: "tumbuhanobat 3",
-            subtitle: "nama latin tumbuhanobat 3",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
+            title: "tumbuhan obat 3",
+            subtitle: "nama latin tumbuhan obat 3",
         },
         {
             href: "/bahan/tumbuhanobat/tumbuhanobat4",
-            img: "Gambar tumbuhanobat 4",
-            title: "tumbuhanobat 4",
-            subtitle: "nama latin tumbuhanobat 4",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
+            title: "tumbuhan obat 4",
+            subtitle: "nama latin tumbuhan obat 4",
         },
         {
             href: "/bahan/tumbuhanobat/tumbuhanobat5",
-            img: "Gambar tumbuhanobat 5",
-            title: "tumbuhanobat 5",
-            subtitle: "nama latin tumbuhanobat 5",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
+            title: "tumbuhan obat 5",
+            subtitle: "nama latin tumbuhan obat 5",
         },
         {
             href: "/bahan/tumbuhanobat/tumbuhanobat6",
-            img: "Gambar tumbuhanobat 6",
-            title: "tumbuhanobat 6",
-            subtitle: "nama latin tumbuhanobat 6",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
+            title: "tumbuhan obat 6",
+            subtitle: "nama latin tumbuhan obat 6",
         },
         {
             href: "/bahan/tumbuhanobat/tumbuhanobat7",
-            img: "Gambar tumbuhanobat 7",
-            title: "tumbuhanobat 7",
-            subtitle: "nama latin tumbuhanobat 7",
+            img: "https://i.postimg.cc/BQPvwSh5/ginger-2022-12-15-21-43-47-utc-transformed-1.png",
+            title: "tumbuhan obat 7",
+            subtitle: "nama latin tumbuhan obat 7",
         },
     ];
 
     return (
         <div className="w-10/12 h-full mx-auto flex flex-col items-center gap-6 font-serif">
-            <span className="text-h3 text-leaf-900 font-bold pt-4">Tumbuhan Obat </span>
+            <span className="self-start text-base"> home/bahan/tumbuhanobat</span>
+            <div className="flex gap-x-2">
+                <Link href="/bahan/tumbuhanobat">
+                    <span className="text-h3 text-leaf-900 font-bold pt-4">Tumbuhan Obat</span>
+                </Link>
+
+                <Link href="/bahan/simplisia">
+                    <span className="text-h3 text-leaf-900 font-bold pt-4">Simplisia</span>
+                </Link>
+            </div>
+
             <div className="grid grid-cols-4 text-xl text-earth-50 gap-6 w-full">
                 {cardData.map((card, index) => (
                     <Card key={index} href={card.href} title={card.title} subtitle={card.subtitle} img={card.img}>
