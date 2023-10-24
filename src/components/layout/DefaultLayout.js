3cd94e0e00969b2daac69c2f8edc050edbdc5409
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-10-24 18:45:08 +0700

    fix: set text white to default

--- src/components/layout/DefaultLayout.js
+++ src/components/layout/DefaultLayout.js
@@ -6,7 +6,7 @@ export default function DefaultLayout({ children, theme = "light" }) {
     return (
         <div className="bg-leaf-950 font-sans w-full min-h-screen">
             <Navbar theme={theme} />
-            <div className="">{children}</div>
+            <div className="text-white">{children}</div>
             {/* <Footer theme={theme} /> */}
         </div>
     );
