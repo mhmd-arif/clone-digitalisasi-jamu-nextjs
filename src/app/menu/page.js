1f521b9f9c0ae7e9947c420f2f1d2f95000968d5
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-10-26 20:00:34 +0700

    hotfix: img package and minor content

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -60,8 +60,7 @@ const LinkCard = ({ href, src, children }) => {
             <Image
                 src={src}
                 fill
-                objectFit="cover"
-                className="brightness-[0.9]"
+                className="object-cover brightness-[0.9]"
                 alt="cover-img"
             />
             <div className="group-hover:-translate-y-3 transition-all relative text-h2 text-center text-leaf-100 font-serif">
--- src/app/video/page.js
+++ src/app/video/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import VideoCard from "./VideoCard";
+import Back from "@/components/Back";
 
 export default function Video() {
     const data = {
@@ -22,13 +23,13 @@ export default function Video() {
                 title: "Teknik Pijat Tradisional untuk Mengatasi Pegal-pegal",
                 description:
                     "Jelajahi teknik pijat tradisional yang dapat membantu mengurangi pegal-pegal dan ketegangan otot.",
-                id: "lZZmlJM7osE",
+                id: "47cz1XE-mzQ",
             },
             {
                 title: "Mengenal Ragam Manfaat Jamu Tradisional untuk Kesehatan",
                 description:
                     "Pelajari beragam manfaat kesehatan yang terkandung dalam jamu tradisional dan bagaimana jamu dapat meningkatkan kesejahteraan Anda.",
-                id: "lZZmlJM7osE",
+                id: "wEaLbnavsmA",
             },
         ],
         videoEdukasi: [
@@ -45,49 +46,26 @@ export default function Video() {
                 id: "lZZmlJM7osE",
             },
             {
-                title: "Tanaman Herbal untuk Menurunkan Kolesterol",
+                title: "Teknik Pijat Tradisional untuk Mengatasi Pegal-pegal",
                 description:
-                    "Pelajari tentang tanaman herbal yang dapat membantu menurunkan kadar kolesterol Anda secara alami",
-                id: "lZZmlJM7osE",
+                    "Jelajahi teknik pijat tradisional yang dapat membantu mengurangi pegal-pegal dan ketegangan otot.",
+                id: "47cz1XE-mzQ",
             },
             {
-                title: "Tanaman Herbal untuk Menurunkan Kolesterol",
+                title: "Mengenal Ragam Manfaat Jamu Tradisional untuk Kesehatan",
                 description:
-                    "Pelajari tentang tanaman herbal yang dapat membantu menurunkan kadar kolesterol Anda secara alami",
-                id: "lZZmlJM7osE",
+                    "Pelajari beragam manfaat kesehatan yang terkandung dalam jamu tradisional dan bagaimana jamu dapat meningkatkan kesejahteraan Anda.",
+                id: "wEaLbnavsmA",
             },
         ],
     };
 
     return (
         <div className="w-full p-24">
+            <Back />
             <h1 className="text-h2 text-center font-serif tracking-widest uppercase mb-8">
                 Video
             </h1>
-            <div className="mb-16">
-                <div className="mb-8">
-                    <h2 className="inline-block text-h3 font-semibold text-earth-200 font-serif">
-                        Video Populer
-                    </h2>
-                    <Link
-                        href={"/video/populer"}
-                        className="ml-6 text-earth-500 hover:text-earth-300 transition-colors"
-                    >
-                        Lihat selengkapnya {">"}
-                    </Link>
-                </div>
-
-                <div className="grid grid-cols-12 gap-12">
-                    <div className="col-span-7">
-                        <VideoCard isPriority data={data.videoEdukasi[0]} />
-                    </div>
-                    <div className="col-span-5 flex flex-col gap-8">
-                        <VideoCard data={data.videoEdukasi[1]} />
-                        <VideoCard data={data.videoEdukasi[2]} />
-                        <VideoCard data={data.videoEdukasi[3]} />
-                    </div>
-                </div>
-            </div>
             <div className="mb-16">
                 <div className="mb-8">
                     <h2 className="inline-block text-h3 font-semibold text-earth-200 font-serif">
@@ -109,6 +87,30 @@ export default function Video() {
                         <VideoCard data={data.videoEdukasi[1]} />
                         <VideoCard data={data.videoEdukasi[2]} />
                         <VideoCard data={data.videoEdukasi[3]} />
+                    </div>
+                </div>
+            </div>
+            <div className="mb-16">
+                <div className="mb-8">
+                    <h2 className="inline-block text-h3 font-semibold text-earth-200 font-serif">
+                        Video Populer
+                    </h2>
+                    <Link
+                        href={"/video/populer"}
+                        className="ml-6 text-earth-500 hover:text-earth-300 transition-colors"
+                    >
+                        Lihat selengkapnya {">"}
+                    </Link>
+                </div>
+
+                <div className="grid grid-cols-12 gap-12">
+                    <div className="col-span-7">
+                        <VideoCard isPriority data={data.videoPopuler[0]} />
+                    </div>
+                    <div className="col-span-5 flex flex-col gap-8">
+                        <VideoCard data={data.videoPopuler[1]} />
+                        <VideoCard data={data.videoPopuler[2]} />
+                        <VideoCard data={data.videoPopuler[3]} />
                     </div>
                 </div>
             </div>
